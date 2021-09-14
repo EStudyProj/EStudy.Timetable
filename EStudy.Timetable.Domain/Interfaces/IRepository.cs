6b0e96c48a72d526cdb73b3d6a8ec4d4dbd0cfe5
Yaroslav Mudryk <yaroslav.mudryk@gmail.com>
2021-09-14 21:32:56 +0300

    Added base model and repository

---
+++ EStudy.Timetable.Domain/Interfaces/IRepository.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+namespace EStudy.Timetable.Domain.Interfaces
+{
+    public interface IRepository<TEntity> where TEntity : class
+    {
+        Task<TEntity> CreateAsync(TEntity entity);
+        Task CreateRangeAsync(List<TEntity> entities);
+        Task<TEntity> UpdateAsync(TEntity entity);
+        Task UpdateRangeAsync(List<TEntity> entities);
+        Task<TEntity> RemoveAsync(TEntity entity);
+        Task RemoveRangeAsync(List<TEntity> entities);
+        Task<List<TEntity>> GetAllAsync(int count, int offset, bool disableTracking = true);
+        Task<TEntity> FirstAsync();
+        Task<TEntity> LastAsync();
+        Task<int> CountAsync();
+        Task<int> CountAsync(Expression<Func<TEntity, bool>> match);
+        Task<long> CountLongAsync();
+        Task<long> CountLongAsync(Expression<Func<TEntity, bool>> match);
+        Task<bool> IsExistAsync(Expression<Func<TEntity, bool>> match);
+        Task<TEntity> FindAsync(Expression<Func<TEntity, bool>> match, bool disableTracking = true);
+        Task<List<TEntity>> FindListAsync(Expression<Func<TEntity, bool>> match, bool disableTracking = true);
+        Task<List<TEntity>> FindListAsync(Expression<Func<TEntity, bool>> match, int count, int skip, bool disableTracking = true);
+    }
+}
---
+++ EStudy.Timetable.Domain/Models/Faculty.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace EStudy.Timetable.Domain.Models
+{
+    public class Faculty : BaseModel<int>
+    {
+        [Required, MinLength(3), MaxLength(250)]
+        public string Name { get; set; }
+        [MinLength(3), MaxLength(250)]
+        public string NameEn { get; set; }
+        public string Description { get; set; }
+    }
+}
