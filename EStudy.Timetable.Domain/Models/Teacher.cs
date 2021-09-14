9e02fddd6bfdbcc71ea0775f223cd29119e5500a
Yaroslav Mudryk <yaroslav.mudryk@gmail.com>
2021-09-14 21:40:21 +0300

    Added teacher model

---
+++ EStudy.Timetable.Domain/Models/Teacher.cs
@@ -0,0 +1,8 @@
+﻿namespace EStudy.Timetable.Domain.Models
+{
+    public class Teacher : BaseModel<int>
+    {
+        public string Name { get; set; }
+        public string NameEng { get; set; }
+    }
+}
