e58b22d8c6771d0b7fcc06e00a001926a1a25f6c
Yaroslav Mudryk <yaroslav.mudryk@gmail.com>
2021-09-30 23:58:53 +0300

    Fix

--- EStudy.Timetable.Domain/Models/Teacher.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace EStudy.Timetable.Domain.Models
-{
-    public class Teacher : BaseModel<int>
-    {
-        public string Name { get; set; }
-        public string NameEng { get; set; }
-    }
-}
